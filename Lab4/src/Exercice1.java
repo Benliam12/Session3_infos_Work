1d3b696202e0621ed5ba1dfeac8a95b29a0eb9f4
Benliam12 <s100live.ca@live.ca>
2019-09-12 10:01:36 -0400

    Exercice 1 fini + Refactoring des noms de fichiers

---
+++ Lab4/src/Exercice1.java
@@ -0,0 +1,62 @@
+import javax.swing.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Exercice1
+{
+    public static void main(String[] args)
+    {
+        int youBadAtEnteringGoodFile = 0;
+
+        while(youBadAtEnteringGoodFile < 3)
+        {
+            String fileName = JOptionPane.showInputDialog("Veuillez entrer le nom du fichier :");
+            fileName += ".txt";
+            try
+            {
+                FileReader fileReader = new FileReader("Lab4/"+fileName);
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+                FileWriter fileWriter = new FileWriter("Lab4/sortie"+fileName);
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+                String line;
+                double sum =0;
+
+                while((line = bufferedReader.readLine()) != null)
+                {
+                    String[] numbers = line.split(" ");
+                    for(String number : numbers)
+                    {
+                        try
+                        {
+                            sum += Double.parseDouble(number);
+                        } catch (NumberFormatException ex)
+                        {
+                            System.out.println(number + " n'est pas un nombre =(");
+                        }
+                    }
+                }
+                bufferedWriter.write(Double.toString(sum));
+                bufferedWriter.close();
+                break;
+            }
+            catch(IOException ex)
+            {
+                youBadAtEnteringGoodFile++;
+                System.out.println("Veuillez entrer un fichier valide! (" + (3-youBadAtEnteringGoodFile) + " tentative(s) restante(s))");
+            }
+            catch (Exception ex)
+            {
+                ex.printStackTrace();
+            }
+            System.out.println(fileName);
+        }
+
+        if(youBadAtEnteringGoodFile == 3)
+            System.out.println("YOU BAD AF");
+    }
+
+}
---
+++ Lab4/src/Exercice2.java
@@ -0,0 +1,2 @@
+public class Exercice2 {
+}
